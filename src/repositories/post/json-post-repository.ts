58647249dcab2d5ee4e3c9b6d516c59bb7d83b02
Alexandre Santos <alexandre.s.o1994@hotmail.com>
2025-10-18 13:06:04 -0300

    fix somethings in json-post-respository

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -12,12 +12,20 @@ const JSON_POSTS_FILE_PATH = resolve(
   'posts.json',
 );
 
+const SIMULATE_AWAIT_IN_MS = 0;
+
 export class JsonPostRepository implements PostRepository {
   private async readFromDisk(): Promise<PostModel[]> {
     const jsonContent = await readFile(JSON_POSTS_FILE_PATH, 'utf-8');
     const parsedJson = JSON.parse(jsonContent);
     const { posts } = parsedJson;
     return posts;
+  }
+
+  private async simulateAwait() {
+    if (SIMULATE_AWAIT_IN_MS <= 0) return;
+
+    await new Promise(resolve => setTimeout(resolve, SIMULATE_AWAIT_IN_MS));
   }
 
   async findById(id: string): Promise<PostModel> {
@@ -29,9 +37,8 @@ export class JsonPostRepository implements PostRepository {
   }
 
   async findAll(): Promise<PostModel[]> {
+    await this.simulateAwait();
     const posts = await this.readFromDisk();
     return posts;
   }
 }
-
-
