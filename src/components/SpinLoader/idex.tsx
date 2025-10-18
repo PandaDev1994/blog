83dd05ef0c262ff81afef881ff1409024838d05b
Alexandre Santos <alexandre.s.o1994@hotmail.com>
2025-10-18 13:06:39 -0300

    create spinLoader component

--- src/components/SpinLoader/idex.tsx
+++ src/components/SpinLoader/idex.tsx
@@ -1,7 +1,13 @@
-'use client';
-export function SpinLoader() {
+import clsx from 'clsx';
+
+type SpinLoaderProps = {
+  className?: string;
+};
+
+export function SpinLoader({ className = '' }: SpinLoaderProps) {
+  const classes = clsx('flex items-center', ' justify-center', className);
   return (
-    <h1 className='flex items-center justify-center  '>
+    <h1 className={classes}>
       <div className='w-10 h-10 border-5 border-t-transparent border-slate-900 rounded-full animate-spin'></div>
     </h1>
   );
