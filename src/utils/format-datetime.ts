6336c78da6deafb7ca49bb68a9fd0cbd4734bd74
Alexandre Santos <alexandre.s.o1994@hotmail.com>
2025-10-21 08:05:03 -0300

    create helper function format-date

---
+++ src/utils/format-datetime.ts
@@ -0,0 +1,23 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
+export function formatDateTime(rowDate: string): string {
+  const date = new Date(rowDate);
+
+  console.log(date);
+
+  return format(date, "dd/MM/yyyy 'às' HH'h'mm", {
+    locale: ptBR,
+  });
+}
+
+export function formatRelativeDate(rowDate: string): string {
+  const date = new Date(rowDate);
+
+  console.log(date);
+
+  return formatDistanceToNow(date, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+}
