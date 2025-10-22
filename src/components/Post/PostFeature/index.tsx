a11bbdeb9e8e2bcce6fa02b863ece1bf69c8c1fa
Alexandre Santos <alexandre.s.o1994@hotmail.com>
2025-10-22 19:07:02 -0300

    put logic of posts in postFeature

--- src/components/Post/PostFeature/index.tsx
+++ src/components/Post/PostFeature/index.tsx
@@ -1,33 +1,31 @@
+import { postRepository } from '@/repositories/post';
 import { PostCoverImage } from '../PostCoverImage';
-import { PostHeading } from '../PostHeading';
+import { PostDescription } from '../PostDescription';
+import { formatDateTime, formatRelativeDate } from '@/utils/format-datetime';
 
-export function PostFeature() {
-  const slug = 'qualquer';
-  const postLink = `/post/${slug}`;
+export async function PostFeature() {
+  const posts = await postRepository.findAllPublic();
+  const post = posts[0];
 
   return (
     <section className='grid grid-cols-1 gap-8 sm:grid-cols-2 mb-16 group'>
       <PostCoverImage
-        linkProps={{ href: postLink }}
+        linkProps={{ href: post.slug }}
         imageProps={{
           width: 1220,
           height: 720,
-          alt: 'titulo',
-          src: '/images/bryen_0.png',
+          alt: post.title,
+          src: post.coverImageUrl,
         }}
       />
-      <div className='flex flex-col gap-2 sm:justify-center'>
-        <time dateTime='2025-4-20' className='text-slate-600 text-sm/tight'>
-          14/04/1994
-        </time>
-        <PostHeading url={postLink} as='h1'>
-          Lorem ipsum dolor sit
-        </PostHeading>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique enim
-        qui laboriosam quaerat eos eligendi iure! Voluptatibus dolorum nesciunt
-        sit similique saepe accusantium iure ducimus consequatur odio. Vero,
-        repellendus voluptatibus.
-      </div>
+      <PostDescription
+        date={formatDateTime(post.updatedAt)}
+        title={post.title}
+        description={post.content}
+        url={''}
+        titleTime={formatRelativeDate(post.createdAt)}
+        as='h1'
+      />
     </section>
   );
 }
