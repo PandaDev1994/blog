d52ac2b570c4d7fc1669d48d96ba4a4bedb77ab9
Alexandre Santos <alexandre.s.o1994@hotmail.com>
2025-10-23 20:52:16 -0300

    put Link instead a

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import Link from 'next/link';
 
 export function Header() {
   return (
@@ -12,7 +13,7 @@ export function Header() {
           'xl:text-8xl xl:py-13',
         )}
       >
-        <a href='#'> The Blog</a>
+        <Link href='/'> The Blog</Link>
       </h1>
     </header>
   );
