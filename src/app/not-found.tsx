a7144a4550d584570d68239bffc566629b087e77
Alexandre Santos <alexandre.s.o1994@hotmail.com>
2025-10-26 12:59:03 -0300

    add title in the notFoundPage

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,17 +2,20 @@ import clsx from 'clsx';
 
 export default function NotFoundPage() {
   return (
-    <div
-      className={clsx(
-        'min-h-[320px] bg-slate-900 text-slate-100 mb-16 p-8 rounded-xl flex items-center justify-center text-center',
-      )}
-    >
-      <div className=' '>
-        <h1 className='text-7xl/tight font-extrabold mb-4'>404</h1>
-        <p>
-          Error 404 - A página que está tentando acessar não existe nesse site
-        </p>
+    <>
+      <title>Página não encontrada</title>
+      <div
+        className={clsx(
+          'min-h-[320px] bg-slate-900 text-slate-100 mb-16 p-8 rounded-xl flex items-center justify-center text-center',
+        )}
+      >
+        <div className=' '>
+          <h1 className='text-7xl/tight font-extrabold mb-4'>404</h1>
+          <p>
+            Error 404 - A página que está tentando acessar não existe nesse site
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
