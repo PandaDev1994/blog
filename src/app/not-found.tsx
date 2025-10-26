de41a295e67bd746cae74f1d6c13cbf1e7272f9c
Alexandre Santos <alexandre.s.o1994@hotmail.com>
2025-10-26 15:07:52 -0300

    change content of the not-foundPage to to use ErrorMessage component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,21 +1,15 @@
-import clsx from 'clsx';
+import ErrorMessage from '@/components/ErrorMessage';
 
 export default function NotFoundPage() {
   return (
     <>
-      <title>Página não encontrada</title>
-      <div
-        className={clsx(
-          'min-h-[320px] bg-slate-900 text-slate-100 mb-16 p-8 rounded-xl flex items-center justify-center text-center',
-        )}
-      >
-        <div className=' '>
-          <h1 className='text-7xl/tight font-extrabold mb-4'>404</h1>
-          <p>
-            Error 404 - A página que está tentando acessar não existe nesse site
-          </p>
-        </div>
-      </div>
+      <ErrorMessage
+        pageTitle={'Página não encontrada'}
+        contentTitle={'404'}
+        content={
+          'Error 404 - A página que está tentando acessar não existe nesse site'
+        }
+      />
     </>
   );
 }
